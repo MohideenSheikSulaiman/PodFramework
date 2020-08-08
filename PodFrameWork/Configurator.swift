e87d4964502c95c833b179c0e95137575059f289
Mohideen Sheik Sulaiman A <mohideen.sulaiman@zohocorp.com>
2020-08-08 18:22:10 +0530

    Initial project setup

---
+++ PodFrameWork/Configurator.swift
@@ -0,0 +1,17 @@
+//
+//  Configurator.swift
+//  PodFrameWork
+//
+//  Created by Mohideen Sheik Sulaiman A on 08/08/20.
+//  Copyright © 2020 Mohideen Sheik Sulaiman A. All rights reserved.
+//
+
+import Foundation
+import FirebaseCore
+
+class Configurator {
+    
+    func configure() {
+        FirebaseApp.configure()
+    }
+}
